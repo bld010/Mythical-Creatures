3cba3038fddc98452561d61f4e1dedaff6492232
Brianna DelValle <briannadelvalle@gmail.com>
2019-05-27 11:05:46 -0600

    Finish scream with rage test

--- exercises/sphinx.js
+++ exercises/sphinx.js
@@ -2,7 +2,7 @@ class Sphinx {
   constructor() {
   this.name = null;
   this.riddles = [];
-  this.heroesEaten = 0;
+  this.heroesEaten = 0
   }
 
 
@@ -14,13 +14,21 @@ class Sphinx {
   }
 
   attemptAnswer(answerGuess) {
-    var answerIndex = this.riddles.indexOf(function(arrayItem) {
-      return arrayItem.answer == answerGuess;
-    })  
-    this.riddles.splice(answerIndex, 1)
-    return 'That wasn\'t that hard, I bet you don\'t get the next one'
-  }
-
+    var answerText = answerGuess;
+    var object = this.riddles.find(function(arrayItem) {
+      return arrayItem.answer === answerGuess; 
+    })
+    if (object === undefined) {
+      this.heroesEaten++;
+      return;
+    }
+    var index = this.riddles.indexOf(object)
+    this.riddles.splice(index, 1); 
+    if (this.riddles.length === 0){
+      return `PSSSSSSS THIS HAS NEVER HAPPENED, HOW DID YOU KNOW THE ANSWER WAS "${answerGuess}"???`
+    }
+    return 'That wasn\'t that hard, I bet you don\'t get the next one'      
+    }
 }
 
 module.exports = Sphinx; 
